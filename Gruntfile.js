1f1872eb9ee0940f8265d3dd701349470a9bc183
Steve Pole <steve.pole@transferwise.com>
2016-12-07 11:09:23 +0000

    Tidy spacing classes, side nav example.  Build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -476,7 +476,8 @@ module.exports = function (grunt) {
           'Element “tw-process” not allowed as child of element “button” in this context. (Suppressing further errors from this subtree.)',
           'Element “topup-form” not allowed as child of element “div” in this context. (Suppressing further errors from this subtree.)',
           'Element “profile-block” not allowed as child of element “div” in this context. (Suppressing further errors from this subtree.)',
-          'Element “simple-nav” not allowed as child of element “div” in this context. (Suppressing further errors from this subtree.)'
+          'Element “simple-nav” not allowed as child of element “div” in this context. (Suppressing further errors from this subtree.)',
+          'Element “side-nav” not allowed as child of element “div” in this context. (Suppressing further errors from this subtree.)'
         ],
         'attr-name-style': false,
         'label-req-for': false,
