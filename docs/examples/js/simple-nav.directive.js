0c8d1a932332aec6f3315ccb9aaeb95fba609ad0
Steve Pole <steve.pole@transferwise.com>
2017-01-06 21:46:31 +0000

    More work on nav

--- docs/examples/js/simple-nav.directive.js
+++ docs/examples/js/simple-nav.directive.js
@@ -8,13 +8,37 @@
 		return {
 			bindToController: true,
 			controller: function() {
-
+				var $ctrl = this;
+				
+				// TODO duplicated from other nav, make service
+				$ctrl.translations = {
+					"en": {
+						"transfers": "Transfers",
+						"history": "History",
+						"recipients": "Recipients",
+						"rewards": "Rewards",
+						"help": "Help",
+						"send1": "Send money",
+						"send2": "Send"
+					},
+					"de": {
+						"transfers": "Überweisungen",
+						"history": "Verlauf",
+						"recipients": "Empfänger",
+						"rewards": "Freunde einladen",
+						"help": "Hilfe",
+						"send1": "Geld überweisen",
+						"send2": "Überweisen"
+					}
+				}
+				$ctrl.language = "en";
 			},
 			controllerAs: '$ctrl',
 			replace: false,
 			restrict: 'E',
 			scope: {
-				onBurgerClick: '&'
+				onBurgerClick: '&',
+				fluid: '='
 			},
 			templateUrl: 'partials/simple-nav.html'
 		}
