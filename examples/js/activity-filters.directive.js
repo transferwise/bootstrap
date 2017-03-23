3318b3bc33ea81320134dc1c086c083771b034c9
Steve Pole <steve.pole@transferwise.com>
2017-03-23 17:12:06 +0000

    Update docs

--- examples/js/activity-filters.directive.js
+++ examples/js/activity-filters.directive.js
@@ -8,18 +8,28 @@
 		return {
 			bindToController: true,
 			controller: function() {
-				this.activityTypes = [
+				var $ctrl = this;
+
+				$ctrl.activityTypes = [
 					{value:'all',label:'All activities',icon:'icon-fast-flag'},
 					{value:'sent',label:'Sent transfers',icon:'icon-send'},
 					{value:'received',label:'Recieved transfers',icon:'icon-request'},
-					{value:'mass-pay',label:'Mass pay batches',icon:'icon-briefcase'}
+					{value:'batch',label:'Mass pay batches',icon:'icon-briefcase'},
+					{value:'request-money',label:'Request money links',icon:'icon-link'},
+					{value:'topup',label:'Balance top ups',icon:'icon-add'},
+					{value:'conversions',label:'Balance conversions',icon:'icon-transfer'},
+					{value:'bonus',label:'Bonus payments',icon:'icon-gift'}
 				];
 			},
 			controllerAs: '$ctrl',
 			replace: true,
 			restrict: 'E',
 			scope: {
-				showSearch: '=',
+				title: "@",
+				buttons: '=',
+				buttonSummary: '@',
+				searchable: '=',
+				filtersActive: '=',
 				size: '@'
 			},
 			templateUrl: 'partials/activity-filters.html'
