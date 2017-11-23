64fd2582e18eae71f73f49f46ea5f818aee85b72
Steve Pole <steve.pole@transferwise.com>
2017-11-23 20:21:36 +0000

    Drop panel padding on larger screens to 24px
    Work on example sites.

---
+++ docs/examples/js/footer.js
@@ -0,0 +1,8 @@
+(function(angular) {
+  'use strict';
+  angular
+    .module('ExampleApp')
+    .component('footerApplication', {
+      templateUrl: "partials/footer.html"
+    });
+})(window.angular);
