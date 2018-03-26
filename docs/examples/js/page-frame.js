01cfff0eeb85ea7f99e5d6576b0b0ed7f8244bc0
Steve Pole <steve.pole@transferwise.com>
2018-03-26 12:57:21 +0100

    adjust nav-invers, nav dropdown position, panel-heading

--- docs/examples/js/page-frame.js
+++ docs/examples/js/page-frame.js
@@ -21,10 +21,11 @@
         };
         $ctrl.sideNav = true;
 
-        if ($ctrl.page === 'activity') {
+        if ($ctrl.page === 'activity' || $ctrl.page === 'borderless-empty') {
           $ctrl.actionButtons = [
             {label: 'Send money', icon: 'icon-send', type: 'success', primary: true}
-            ,{label: 'Add money', icon: 'icon-add'}
+            //,{label: 'Get the card', icon: 'icon-card'}
+            //,{label: 'Add money', icon: 'icon-add'}
           ];
         } else if ($ctrl.page === 'balance') {
           $ctrl.actionButtons = [
@@ -36,11 +37,19 @@
             {label: 'Add account', icon: 'icon-send', type: 'success', primary: true}
             ,{label: 'Add recipient', icon: 'icon-add'}
           ];
+        } else if ($ctrl.page === 'card') {
+          $ctrl.actionButtons = [
+            {label: 'Freeze your card', icon: 'icon-card'}
+            ,{label: 'Reveal your pin', icon: 'icon-card'}
+            ,{label: 'Report a problem', icon: 'icon-card'}
+            //,{label: 'Activate your card', icon: 'icon-card', type: 'success', primary: true}
+          ];
         }
 
-        /**
+        /**/
         var now = new Date();
         var after = new Date(now.getTime() + (10 * 1000));
+        /**
         NavTourService.addSimpleTour(
           'inviteFriends',
           'right',
@@ -53,11 +62,24 @@
         );
 
         NavTourService.addSimpleTour(
+          'card',
+          'right',
+          'Did you know?',
+          'By inviting your friends you can earn free transfers. Invite 3 friends to earn £50 off your fees.',
+          '/assets/img/css2-bg.jpg',
+          '/invite',
+          'Invite now'
+          //,after
+        );
+        /**
+
+
+        NavTourService.addSimpleTour(
           'profileSelect',
-          'bottom',
-          'You now have two profiles',
-          'Make sure to choose the correct profile when sending money.',
-          '/assets/img/css2-bg.jpg'
+          'right',
+          'Get the card!',
+          'Sign up for a borderless account to recieve money from abroad, and to spend anywhere in the world without hidden fees.',
+          'https://lienzo.s3.amazonaws.com/images/TransferWisecard4@2x.png'
         );
 
         NavTourService.checkAndShowTour();
